e4956e0ae7d98ff28bb48da140bfd2a63e3904ea
asforest <asforest@qq.com>
2024-05-13 20:29:40 +0800

    构建的pe文件增加版本号

---
+++ mcpatch-client/build.rs
@@ -0,0 +1,20 @@
+use std::path::PathBuf;
+
+fn main() {
+    let rc_file = PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("pe.rc");
+
+    println!("cargo:rerun-if-changed={}", rc_file.to_str().unwrap());
+
+    let major = env!("CARGO_PKG_VERSION_MAJOR");
+    let minor = env!("CARGO_PKG_VERSION_MINOR");
+    let patch = env!("CARGO_PKG_VERSION_PATCH");
+    let pre = if env!("CARGO_PKG_VERSION_PRE").is_empty() { "0" } else { env!("CARGO_PKG_VERSION_PRE") };
+
+    println!("1 VERSIONINFO FILEVERSION {major},{minor},{patch},{pre} {{ }}");
+
+    let rc_content = format!("1 VERSIONINFO FILEVERSION {major},{minor},{patch},{pre} {{ }}");
+
+    std::fs::write(&rc_file, rc_content).unwrap();
+
+    embed_resource::compile(&rc_file, embed_resource::NONE);
+}
