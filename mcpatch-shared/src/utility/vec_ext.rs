5edd3a04774510d0c8e3e0c76383d2e5da6ef27e
asforest <asforest@qq.com>
2024-06-29 13:36:02 +0800

    优化一处小代码

--- mcpatch-shared/src/utility/vec_ext.rs
+++ mcpatch-shared/src/utility/vec_ext.rs
@@ -1,11 +1,11 @@
 /// 按条件删除Vec<T>中的元素，会从末尾往前删以避免元素移动
 pub trait VecRemoveIf<T> {
-    fn remove_if(&mut self, f: impl Fn(&T) -> bool);
+    fn remove_if(&mut self, f: impl FnMut(&T) -> bool);
 }
 
 impl<T> VecRemoveIf<T> for Vec<T> {
     /// 按条件删除元素，如果要删除某个元素请返回true，反之返回false
-    fn remove_if(&mut self, f: impl Fn(&T) -> bool) {
+    fn remove_if(&mut self, mut f: impl FnMut(&T) -> bool) {
         for i in (0..self.len()).rev() {
             if f(&self[i]) {
                 self.remove(i);
