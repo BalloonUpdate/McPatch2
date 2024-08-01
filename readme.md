dd0d55263c88e0b0c06298312e1b4728c10cd11c
asforest <asforest@qq.com>
2024-08-01 13:03:25 +0800

    使用新的构建发布流程

--- readme.md
+++ readme.md
@@ -1,18 +1,25 @@
 ### McPatch2
 
-McPatch第二版，包含客户端和管理端的源代码，目前还在早期开发阶段
+McPatch第二版，包含客户端和管理端的源代码。
 
-crates说明：
+### crates说明
 
-+ config_template_derive：给客户端用的过程宏，用来根据源代码注释自动化生成客户端配置文件模板
-+ mcpatch-client：客户端程序，用来执行文件更新过程，需要配置好后分发给玩家（二进制crate）
-+ mcpatch-manager：管理端程序，负责更新包的打包和管理工作，也提供内置开箱即用的内置服务端（二进制crate）
-+ mcpatch-shared：客户端和管理端共用的代码部分
+| 名称                   | 用途                                                         |
+| ---------------------- | ------------------------------------------------------------ |
+| mcpatch-client         | 客户端主程序。用来执行文件更新过程，需要配置好后分发给玩家   |
+| mcpatch-manager        | 管理端主程序。负责更新包的打包和管理工作，也提供内置开箱即用的内置服务端 |
+| config_template_derive | 给客户端用的过程宏，用来根据源代码注释自动化生成客户端配置文件模板 |
+| mcpatch-shared         | 客户端和管理端共用的代码部分                                 |
+| xtask                  | 用于ci/cd自动化打包的行为和命令                              |
 
-常用命令说明：
+### 常用命令说明
 
-+ 执行构建检查：`cargo check --all`
-+ 构建客户端可执行文件：`cargo build --bin mcpatch-client`
-+ 构建管理端可执行文件：`cargo build --bin mcpatch-manager`
-+ 生成文档页面：`cargo doc --no-deps --open`
-
+| 命令                                | 作用                                 |
+| ----------------------------------- | ------------------------------------ |
+| `cargo cc`                          | 开发场景下，启动客户端程序           |
+| `cargo mm`                          | 开发场景下，启动管理端程序           |
+| `cargo check --all`                 | 开发场景下，执行构建检查             |
+| `cargo build --bin mcpatch-client`  | 开发场景下，打包客户端程序           |
+| `cargo build --bin mcpatch-manager` | 开发场景下，打包管理端程序           |
+| `cargo doc --no-deps --open`        | 开发场景下，生成文档页面             |
+| `cargo ci <client/manager>`         | 自动构建场景下，打包客户端或者管理端 |
---
+++ xtask/src/main.rs
@@ -0,0 +1,102 @@
+use std::path::Path;
+use std::path::PathBuf;
+use std::process::Command;
+
+type ProcessResult = Result<(), Box<dyn std::error::Error>>;
+
+fn main() -> ProcessResult {
+    let task = std::env::args().nth(1);
+
+    match task.as_deref() {
+        Some("client") => dist_binary("mcpatch-client"),
+        Some("manager") => dist_binary("mcpatch-manager"),
+        _ => print_help(),
+    }
+}
+
+fn dist_binary(crate_name: &str) -> ProcessResult {
+    std::env::set_var("RUST_BACKTRACE", "1");
+
+    let ref_name = github_ref_name();
+    let dist_dir = project_root().join("target/dist");
+    let target = TargetInfo::get(crate_name, &ref_name, &dist_dir);
+
+    // build artifacts
+    let cargo = std::env::var("CARGO").unwrap();
+
+    let status = Command::new(cargo)
+        .current_dir(project_root())
+        .args(&["build", "--release", "--bin", crate_name, "--target", &target.rustc_target])
+        .status()?;
+
+    if !status.success() {
+        Err("cargo build failed")?;
+    }
+
+    // pick up artifacts
+    drop(std::fs::remove_dir_all(&dist_dir));
+    std::fs::create_dir_all(&dist_dir).unwrap();
+
+    // executable
+    std::fs::copy(&target.artifact_path, dist_dir.join(&target.artifact_path_versioned)).unwrap();
+
+    // symbol
+    if let Some(symbols) = target.symbols_path {
+        std::fs::copy(&symbols, dist_dir.join(&target.symbols_path_versioned.unwrap())).unwrap();
+    }
+
+    Ok(())
+}
+
+fn print_help() -> ProcessResult {
+    return Err("use 'client' or 'manager'")?;
+}
+
+fn project_root() -> PathBuf {
+    Path::new(&env!("CARGO_MANIFEST_DIR"))
+        .ancestors()
+        .nth(1)
+        .unwrap()
+        .to_path_buf()
+}
+
+fn github_ref_name() -> String {
+    std::env::var("GITHUB_REF_NAME").map(|e| e[1..].to_owned()).unwrap_or("0.0.0".to_owned())
+}
+
+struct TargetInfo {
+    rustc_target: String,
+
+    artifact_path: PathBuf,
+    symbols_path: Option<PathBuf>,
+
+    artifact_path_versioned: PathBuf,
+    symbols_path_versioned: Option<PathBuf>,
+}
+
+impl TargetInfo {
+    fn get(crate_name: &str, version_label: &str, dist_dir: &Path) -> Self {
+        let rustc_target = std::env::var("MP_RUSTC_TARGET").unwrap();
+        let profile_path = project_root().join(format!("target/{}/release", &rustc_target));
+        let is_windows = rustc_target.contains("-windows-");
+
+        let (exe_suffix, symbols_suffix) = match is_windows {
+            true => (".exe", Some(".pdb")),
+            false => ("", None),
+        };
+
+        let symbols_name = symbols_suffix.map(|e| format!("{}{e}", crate_name.replace("-", "_")));
+        let symbols_name_versioned = symbols_suffix.map(|e| format!("{crate_name}-{version_label}-{rustc_target}{e}"));
+
+        let artifact_name = format!("{crate_name}{exe_suffix}");
+        let artifact_name_versioned = format!("{crate_name}-{version_label}-{rustc_target}{exe_suffix}");
+
+        let artifact_path = profile_path.join(&artifact_name);
+        let symbols_path = symbols_name.as_ref().map(|e| profile_path.join(e));
+        
+        let artifact_path_versioned = dist_dir.join(&artifact_name_versioned);
+        let symbols_path_versioned = symbols_name_versioned.as_ref().map(|e| dist_dir.join(e));
+
+        Self { rustc_target, artifact_path, symbols_path, artifact_path_versioned, symbols_path_versioned }
+    }
+}
