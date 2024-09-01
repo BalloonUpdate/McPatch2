7cdf43f8a1030c42d969ba7e4302b451cf80b254
asforest <asforest@qq.com>
2024-09-01 10:57:29 +0800

    Cargo: 尝试缩短构建输出文件名

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -8,26 +8,26 @@ fn main() -> ProcessResult {
     let task = std::env::args().nth(1);
 
     match task.as_deref() {
-        Some("client") => dist_binary("mcpatch-client"),
-        Some("manager") => dist_binary("mcpatch-manager"),
+        Some("client") => dist_binary("mcpatch-client", "client"),
+        Some("manager") => dist_binary("mcpatch-manager", "manager"),
         _ => print_help(),
     }
 }
 
-fn dist_binary(crate_name: &str) -> ProcessResult {
+fn dist_binary(crate_name: &str, production_name: &str) -> ProcessResult {
     std::env::set_var("RUST_BACKTRACE", "1");
 
     let ref_name = github_ref_name();
     let dist_dir = project_root().join("target/dist");
-    let target = TargetInfo::get(crate_name, &ref_name, &dist_dir);
+    let target = TargetInfo::get(crate_name, production_name, &ref_name, &dist_dir);
 
     // build artifacts
     let cargo = std::env::var("CARGO").unwrap();
 
-    let status = Command::new(cargo)
-        .current_dir(project_root())
-        .args(&["build", "--release", "--bin", crate_name, "--target", &target.rustc_target])
-        .status()?;
+    let mut cmd = Command::new(cargo);
+    cmd.current_dir(project_root());
+    cmd.args(&["build", "--release", "--bin", crate_name, "--target", &target.rustc_target]);
+    let status = cmd.status()?;
 
     if !status.success() {
         Err("cargo build failed")?;
@@ -75,8 +75,21 @@ struct TargetInfo {
 }
 
 impl TargetInfo {
-    fn get(crate_name: &str, version_label: &str, dist_dir: &Path) -> Self {
-        let rustc_target = std::env::var("MP_RUSTC_TARGET").unwrap();
+    fn get(crate_name: &str, production_name: &str, version_label: &str, dist_dir: &Path) -> Self {
+        let rustc_target = match std::env::var("MP_RUSTC_TARGET") {
+            Ok(t) => t,
+            Err(_) => {
+                if cfg!(target_os = "linux") {
+                    "x86_64-unknown-linux-gnu".to_owned()
+                } else if cfg!(target_os = "windows") {
+                    "x86_64-pc-windows-msvc".to_owned()
+                } else if cfg!(target_os = "macos") {
+                    "x86_64-apple-darwin".to_owned()
+                } else {
+                    panic!("Unsupported OS, maybe try setting MP_RUSTC_TARGET")
+                }
+            },
+        };
         let profile_path = project_root().join(format!("target/{}/release", &rustc_target));
         let is_windows = rustc_target.contains("-windows-");
 
@@ -86,10 +99,10 @@ impl TargetInfo {
         };
 
         let symbols_name = symbols_suffix.map(|e| format!("{}{e}", crate_name.replace("-", "_")));
-        let symbols_name_versioned = symbols_suffix.map(|e| format!("{crate_name}-{version_label}-{rustc_target}{e}"));
+        let symbols_name_versioned = symbols_suffix.map(|e| format!("{production_name}-{version_label}-{rustc_target}{e}"));
 
         let artifact_name = format!("{crate_name}{exe_suffix}");
-        let artifact_name_versioned = format!("{crate_name}-{version_label}-{rustc_target}{exe_suffix}");
+        let artifact_name_versioned = format!("{production_name}-{version_label}-{rustc_target}{exe_suffix}");
 
         let artifact_path = profile_path.join(&artifact_name);
         let symbols_path = symbols_name.as_ref().map(|e| profile_path.join(e));
