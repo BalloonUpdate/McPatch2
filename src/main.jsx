16039fc0453616433c6a9aae1b7d8e6aba1a196b
Sengimu <JSengimu@gmail.com>
2024-11-25 17:38:27 +0800

    style: 修改antd主题色

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,14 @@ import {Provider} from "react-redux";
 
 import router from "@/router/index.jsx";
 import store from "@/store/index.js";
+import {ConfigProvider} from "antd";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router}/>
-    </Provider>
+    <ConfigProvider theme={{token: {colorPrimary: '#4f46e5'}}}>
+      <Provider store={store}>
+        <RouterProvider router={router}/>
+      </Provider>
+    </ConfigProvider>
   </StrictMode>
 )
