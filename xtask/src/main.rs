edb694a0450e124ce7f641ea9aeb5760d4c05110
asforest <asforest@qq.com>
2024-12-09 14:14:46 +0800

    q

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -2,17 +2,57 @@ use std::path::Path;
 use std::path::PathBuf;
 use std::process::Command;
 
+use zip::ZipArchive;
+
 type ProcessResult = Result<(), Box<dyn std::error::Error>>;
 
 fn main() -> ProcessResult {
     let mut args = std::env::args();
     let task = args.next();
-    let features = args.next();
 
     match task.as_deref() {
-        Some("client") => dist_binary("client", "c", features),
-        Some("manager") => dist_binary("manager", "m", features),
+        Some("client") => dist_client(),
+        Some("manager") => dist_manager(),
         _ => print_help(),
+    }
+}
+
+fn dist_client() -> ProcessResult {
+    dist_binary("client", "c", None)
+}
+
+fn dist_manager() -> ProcessResult {
+    let version_label = &std::env::var("GITHUB_REF_NAME").unwrap()[1..];
+
+    let dist_url = format!("https://github.com/BalloonUpdate/McPatch2Web/releases/download/v{}/dist.zip", version_label);
+    let response = reqwest::blocking::get(dist_url).unwrap();
+
+    std::fs::write("webpage.zip", response.bytes().unwrap()).unwrap();
+
+    std::fs::create_dir_all("test/webpage").unwrap();
+
+    unzip_file("webpage.zip", "test/webpage");
+
+    dist_binary("manager", "m", Some("bundle-webpage".to_owned()))
+}
+
+fn unzip_file(zip_path: &str, output_dir: &str) {
+    let file = std::fs::File::open(zip_path).unwrap();
+    let mut archive = ZipArchive::new(file).unwrap();
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i).unwrap();
+        let outpath = std::path::Path::new(output_dir).join(file.name());
+
+        if file.is_dir() {
+            std::fs::create_dir_all(&outpath).unwrap();
+        } else {
+            if let Some(p) = outpath.parent() {
+                std::fs::create_dir_all(p).unwrap();
+            }
+            let mut outfile = std::fs::File::create(&outpath).unwrap();
+            std::io::copy(&mut file, &mut outfile).unwrap();
+        }
     }
 }
 
