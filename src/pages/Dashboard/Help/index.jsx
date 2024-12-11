2c8c2fc49594e1f1c4d4b189753d7a93850a7f95
Sengimu <JSengimu@gmail.com>
2024-12-11 08:37:06 +0800

    feat: 帮助页增加文档指引

--- src/pages/Dashboard/Help/index.jsx
+++ src/pages/Dashboard/Help/index.jsx
@@ -1,7 +1,15 @@
+import {SquareArrowOutUpRight} from "lucide-react";
+
 const Index = () => {
   return (
     <>
-      帮助
+      <div className="flex items-center mt-2 text-base font-bold text-indigo-600">
+        <span>官方文档:</span>
+        <a className="flex items-center ml-2 text-gray-500 w-40" target="_blank"
+           href="https://balloonupdate.github.io/McPatchDocs/">
+          [打开<SquareArrowOutUpRight size={12} strokeWidth={1.5}/>]
+        </a>
+      </div>
     </>
   );
 };
